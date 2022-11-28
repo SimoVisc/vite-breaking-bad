7eb63cdbc9a12d09f268aba38161fb247edd6d83
SimoVisc <simonaviscardivs@gmail.com>
2022-11-28 19:37:03 +0100

    aggiunge app Card

---
+++ src/components/AppCard.vue
@@ -0,0 +1,20 @@
+<script>
+import { objectToString } from '@vue/shared';
+
+    export default {
+    name: "AppCard",
+    props:{
+        info: Object;
+    }
+   }
+</script>
+
+<template>
+    
+</template>
+
+<style lang="scss">
+img{
+    width:100%
+}
+</style>
--- src/components/SectionCharacters.vue
+++ src/components/SectionCharacters.vue
@@ -1,11 +1,19 @@
 <script>
+    import AppCard from './AppCard.vue';
     export default {
     name: "SectionCharacters",
+    components: {
+        AppCard
+    }
    }
 </script>
 
 <template>
-   
+   <section class="container">
+    <div class="row">
+       <AppCard class="col-auto" />
+    </div>
+   </section>
 </template>
 
 <style lang="scss">
