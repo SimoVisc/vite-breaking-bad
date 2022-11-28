c9a5faf9bd871f7150b3229f57ab3c36fc20aef1
SimoVisc <simonaviscardivs@gmail.com>
2022-11-28 14:28:54 +0100

    Aggiunge struutura di base

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+    import AppLogo from './AppLogo.vue';
+
+    export default {
+        name: "AppHeader",
+        components:{
+            AppLogo
+        }
+    }
+</script>
+ 
+<template>
+  <header class="container">
+     <AppLogo/>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+ 
+</style>
---
+++ src/components/AppLogo.vue
@@ -0,0 +1,12 @@
+<script>
+    export default {
+    name: "AppLogo",
+   }
+</script>
+
+<template>
+  <img src="../assets/img/logo.png" alt="logo">
+</template>
+
+<style lang="scss">
+</style>
