44a6891d8833375f4bf16fd8857d473c4f3c4ada
SimoVisc <simonaviscardivs@gmail.com>
2022-11-28 18:57:15 +0100

    Aggiunge search bar

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,12 +1,18 @@
 <script>
+    import SearchBar from './searchBar.vue';
     export default {
     name: "AppMain",
+    components: {
+      SearchBar
+    }
    }
 </script>
 
 <template>
     <main class="container">
-      <div>Select Category</div>
+      <div class="searchbar-container">
+          <SearchBar/>
+      </div>
       <div class="card-container">
         <h2>Found 62 characters</h2>
       </div>
@@ -14,12 +20,16 @@
 </template>
 
 <style lang="scss" scoped>
+.searchbar-container{
+   width: 25%;
+   padding:20px;
+}
  .card-container{
     background-color: #fff;
     padding: 5px;
     h2{
-    background-color: var(--secondary-color);
-    color: #fff;
+    background-color: $secondary-color;
+  
     padding:10px
     }
  }
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+    name: "SearchBar",
+   }
+</script>
+
+<template>
+    <select class="form-select searchbar" aria-label="Default select example">
+        <option selected>Select Category</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+    </select>
+</template>
+
+<style lang="scss">
+.searchbar{
+width: 200px;
+}
+</style>
