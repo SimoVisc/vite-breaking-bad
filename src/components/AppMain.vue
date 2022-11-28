80734ce47c252047a968a59264b6d3ac2f97224f
SimoVisc <simonaviscardivs@gmail.com>
2022-11-28 15:05:27 +0100

    Aggiunge main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -5,12 +5,23 @@
 </script>
 
 <template>
-    <main>
-      
+    <main class="container">
+      <div>Select Category</div>
+      <div class="card-container">
+        <h2>Found 62 characters</h2>
+      </div>
    </main>
 </template>
 
 <style lang="scss" scoped>
- 
+ .card-container{
+    background-color: #fff;
+    padding: 5px;
+    h2{
+    background-color: var(--secondary-color);
+    color: #fff;
+    padding:10px
+    }
+ }
 
 </style>
